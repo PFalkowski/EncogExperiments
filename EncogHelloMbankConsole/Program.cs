9e4800a19ef066d573ada565c697ef3fe32a9d06
PFalkowki <Piotr.Falkowski.fm@gmail.com>
2018-02-11 00:39:17 +0100

    adjust console app to changes

--- EncogHelloMbankConsole/Program.cs
+++ EncogHelloMbankConsole/Program.cs
@@ -1,100 +1,120 @@
-﻿namespace EncogHelloMbankConsole
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Encog.Engine.Network.Activation;
+using Encog.ML.Data.Basic;
+using Encog.Neural.Networks;
+using Encog.Neural.Networks.Layers;
+using Encog.Neural.Networks.Training.Propagation.Resilient;
+using Extensions.Standard;
+using LoggerLite;
+using StocksData.Adapters;
+using StocksData.Contexts;
+using StocksData.Mappings;
+using StocksData.Model;
+using StocksData.Services;
+using StocksData.UnitsOfWork;
+
+namespace EncogHelloMbankConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //var consoleLogger = new ConsoleLogger
-            //{
-            //    InfoColor = ConsoleColor.Gray
-            //};
-            //var fileLogger = new FileLoggerBase("console");
-            //var logger = new AggregateLogger(consoleLogger, fileLogger); 
+            var logger = new AggregateLogger(new ConsoleLogger { InfoColor = ConsoleColor.Gray}, new FileLoggerBase("console"));
 
-            //var ommitStocksLessThanDays = 200;
-            //CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
-            //var timer = Stopwatch.StartNew();
+            var ommitStocksLessThanDays = 200;
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+            var timer = Stopwatch.StartNew();
 
-            //const string connectionStr = @"server=(localdb)\MSSQLLocalDB;Initial Catalog=StockMarketDb;Integrated Security=True;";
-            //var unitOfWork = new StockDatabaseUnitOfWork(new StockDbContext(connectionStr));
+            const string connectionStr = @"server=(localdb)\MSSQLLocalDB;Initial Catalog=StockMarketDb;Integrated Security=True;";
+            var unitOfWork = new StockEfUnitOfWork(new StockEfContext(connectionStr));
 
-            ////var allFiles = new IOService().ReadDirectory("C:\\Users\\John\\Downloads\\mstcgl", "*.mst");
-            ////logger.LogInfo($@"Read {allFiles.Count} files in {timer.ElapsedMilliseconds.AsTime()}");
+            //var allFiles = new IOService().ReadDirectory("C:\\Users\\John\\Downloads\\mstcgl", "*.mst");
+            //logger.LogInfo($@"Read {allFiles.Count} files in {timer.ElapsedMilliseconds.AsTime()}");
 
-            //logger.LogInfo($@"Initiated db connection in {timer.ElapsedMilliseconds.AsTime()}");
+            logger.LogInfo($@"Initiated db connection in {timer.ElapsedMilliseconds.AsTime()}");
 
-            //timer.Restart();
-            //var allStocks = new List<List<StockQuote>>(allFiles.Count);
+            timer.Restart();
 
-            //Parallel.ForEach(allFiles, (file) => allStocks.Add(file.Value.DeserializeFromCsv(new StockQuoteCsvClassMap()).ToList()));
-            //logger.LogInfo($@"Deserialized {allStocks.Count} in {timer.ElapsedMilliseconds.AsTime()}");
+            var directoryService = new IOService();
+            var stocksRaw = directoryService.ReadDirectory(@"C:\Users\John\Downloads\mstcgl");
 
-            //timer.Restart();
-            //var normalizer = new StockQuotesToNormalizedMatrix();
+            logger.LogInfo($@"Read {stocksRaw.Count} in {timer.ElapsedMilliseconds.AsTime()}");
 
-            //var allStocksNormalized = new List<BasicMLDataSet>();
-            //var matrixConverter = new MatrixToMLData();
-            //Parallel.ForEach(allStocks, (stock) =>
-            //{
-            //    if (stock.Count >= ommitStocksLessThanDays)
-            //        allStocksNormalized.Add(matrixConverter.ConvertToHighPred(normalizer.Convert(stock)));
-            //});
+            var stocksDeserialized = new StocksBulkDeserializer().Deserialize(stocksRaw);
 
-            //logger.LogInfo($@"Converted and normalized {allStocksNormalized.Count} in {timer.ElapsedMilliseconds.AsTime()}. Ommited {allStocks.Count - allStocksNormalized.Count} for reson: less than {ommitStocksLessThanDays} samples.");
+            logger.LogInfo($@"Deserialized {stocksDeserialized.Count} in {timer.ElapsedMilliseconds.AsTime()}");
 
-            //timer.Restart();
-            //var oneDataSet = new BasicMLDataSet();
-            //foreach (var stockNormal in allStocksNormalized)
-            //{
-            //    foreach (var mlDataPair in stockNormal.Data)
-            //    {
-            //        oneDataSet.Add(mlDataPair);
-            //    }
-            //}
-            //logger.LogInfo($@"Constructed dataset with {oneDataSet.Count} samples in {timer.ElapsedMilliseconds.AsTime()}");
+            timer.Restart();
+            var normalizer = new StockQuotesToNormalizedMatrix();
 
-            //var network = new BasicNetwork();
-            //network.AddLayer(new BasicLayer(null, true, 5));
+            var allStocksNormalized = new List<BasicMLDataSet>();
+            var matrixConverter = new MatrixToMLData();
+            Parallel.ForEach(stocksDeserialized, (stock) =>
+            {
+                if (stock.Quotes.Count >= ommitStocksLessThanDays)
+                    allStocksNormalized.Add(matrixConverter.ConvertToHighPred(normalizer.Convert(stock.Quotes.ToList())));
+            });
+
+            logger.LogInfo($@"Converted and normalized {allStocksNormalized.Count} in {timer.ElapsedMilliseconds.AsTime()}. Ommited {stocksDeserialized.Count - allStocksNormalized.Count}. Reson: less than {ommitStocksLessThanDays} samples.");
+
+            timer.Restart();
+            var oneDataSet = new BasicMLDataSet();
+            foreach (var stockNormal in allStocksNormalized)
+            {
+                foreach (var mlDataPair in stockNormal.Data)
+                {
+                    oneDataSet.Add(mlDataPair);
+                }
+            }
+            logger.LogInfo($@"Constructed dataset with {oneDataSet.Count} samples in {timer.ElapsedMilliseconds.AsTime()}");
+
+            var network = new BasicNetwork();
+            network.AddLayer(new BasicLayer(null, true, 5));
+            network.AddLayer(new BasicLayer(new ActivationClippedLinear(), true, 230));
+            //network.AddLayer(new BasicLayer(new ActivationClippedLinear(), true, 310));
             //network.AddLayer(new BasicLayer(new ActivationClippedLinear(), true, 230));
-            ////network.AddLayer(new BasicLayer(new ActivationClippedLinear(), true, 310));
-            ////network.AddLayer(new BasicLayer(new ActivationClippedLinear(), true, 230));
-            //network.AddLayer(new BasicLayer(new ActivationClippedLinear(), true, 23));
-            //network.AddLayer(new BasicLayer(new ActivationClippedLinear(), true, 23));
-            //network.AddLayer(new BasicLayer(new ActivationClippedLinear(), true, 23));
-            //network.AddLayer(new BasicLayer(new ActivationClippedLinear(), true, 23));
-            //network.AddLayer(new BasicLayer(new ActivationClippedLinear(), true, 23));
-            //network.AddLayer(new BasicLayer(new ActivationClippedLinear(), true, 5));
-            //network.AddLayer(new BasicLayer(new ActivationClippedLinear(), false, 1));
+            network.AddLayer(new BasicLayer(new ActivationClippedLinear(), true, 23));
+            network.AddLayer(new BasicLayer(new ActivationClippedLinear(), true, 23));
+            network.AddLayer(new BasicLayer(new ActivationClippedLinear(), true, 23));
+            network.AddLayer(new BasicLayer(new ActivationClippedLinear(), true, 23));
+            network.AddLayer(new BasicLayer(new ActivationClippedLinear(), true, 23));
+            network.AddLayer(new BasicLayer(new ActivationClippedLinear(), true, 5));
+            network.AddLayer(new BasicLayer(new ActivationClippedLinear(), false, 1));
 
 
-            //network.Structure.FinalizeStructure();
-            //network.Reset();
+            network.Structure.FinalizeStructure();
+            network.Reset();
 
-            ////var train = new Backpropagation(network, trainingSet, 0.07, 0.07);
-            //var train = new ResilientPropagation(network, oneDataSet);
+            //var train = new Backpropagation(network, trainingSet, 0.07, 0.07);
+            var train = new ResilientPropagation(network, oneDataSet);
 
-            //var learningTime = default(TimeSpan);
-            //do
-            //{
-            //    logger.LogInfo(@"Training... ");
-            //    train.Iteration();
-            //    logger.LogInfo($@"iteration {train.IterationNumber} completed in {timer.ElapsedMilliseconds.AsTime()}; error = {train.Error}");
-            //    learningTime += timer.Elapsed;
-            //    timer.Restart();
-            //} while (train.Error > 0.02);
-            //logger.LogInfo($@"Training finished after {learningTime.TotalMilliseconds.AsTime()}, at iteration {train.IterationNumber}; error = {train.Error}");
+            var learningTime = default(TimeSpan);
+            do
+            {
+                logger.LogInfo(@"Training... ");
+                train.Iteration();
+                logger.LogInfo($@"iteration {train.IterationNumber} completed in {timer.ElapsedMilliseconds.AsTime()}; error = {train.Error}");
+                learningTime += timer.Elapsed;
+                timer.Restart();
+            } while (train.Error > 0.02);
+            logger.LogInfo($@"Training finished after {learningTime.TotalMilliseconds.AsTime()}, at iteration {train.IterationNumber}; error = {train.Error}");
 
-            //var testSet = oneDataSet.Skip(1200).ToList().Take(200).ToList();
-            //var avgError = 0.0;
-            //foreach (var testSample in testSet)
-            //{
-            //    var output = network.Compute(testSample.Input);
-            //    var difference = testSample.Ideal[0] - output[0];
-            //    avgError += Math.Abs(difference);
-            //}
-            
-            //logger.LogInfo($@"evaluation of training data resulted in avgError = {avgError}");
-            //Console.Read();
+            var testSet = oneDataSet.Skip(1200).ToList().Take(200).ToList();
+            var avgError = 0.0;
+            foreach (var testSample in testSet)
+            {
+                var output = network.Compute(testSample.Input);
+                var difference = testSample.Ideal[0] - output[0];
+                avgError += Math.Abs(difference);
+            }
+
+            logger.LogInfo($@"evaluation of training data resulted in avgError = {avgError}");
+            Console.Read();
         }
     }
 }
---
+++ StocksData/Services/StocksBulkDeserializer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Extensions.Serialization;
+using StocksData.Mappings;
+using StocksData.Model;
+
+namespace StocksData.Services
+{
+    public class StocksBulkDeserializer
+    {
+        public List<Company> Deserialize(Dictionary<string, string> files)
+        {
+            var allStocks = new List<Company>(files.Count);
+
+            Parallel.ForEach(files,
+                delegate (KeyValuePair<string, string> file)
+                {
+                    var deserializedQuotes = file.Value.DeserializeFromCsv(new StockQuoteCsvClassMap(), CultureInfo.InvariantCulture).ToList();
+                    var companyName = Path.GetFileNameWithoutExtension(file.Key);
+                    if (companyName != deserializedQuotes.First().Ticker) throw new Exception($"{companyName} != {deserializedQuotes.First().Ticker}");
+
+                    allStocks.Add(new Company
+                    {
+                        Ticker = companyName,
+                        Quotes = deserializedQuotes
+                    });
+                });
+
+            return allStocks;
+        }
+    }
+}
