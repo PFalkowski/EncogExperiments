d2fcc9cc4bef90d3c8d56c422c64691ca2b7327e
Piotr Falkowski <falcon777@wp.pl>
2018-04-09 20:41:24 +0200

    Install Effort. Mess with references.

--- Services/IOService.cs
+++ Services/IOService.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using Infrastracture;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
 namespace Services
 {
-    public class IOService
+    public class IOService// : IOService
     {
         public Dictionary<string, string> ReadDirectory(DirectoryInfo dir, string pattern = "*.*", SearchOption searchDepth = SearchOption.TopDirectoryOnly)
         {
